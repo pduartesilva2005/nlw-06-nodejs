d7a99a69724466ff46a715bc21df478f7334c40a
unknown <pduartesilva2005@gmail.com>
2021-09-21 19:44:24 -0300

    Day 03 - In Orbit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
+import 'express-async-errors';
 
 import './database';
 import { routes } from './routes';
+import { errorHandler } from './errors/handler';
 
 export const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/CreateUserController';
+import { CreateTagController } from './controllers/CreateTagController';
+import { ensureAdmin } from './middlewares/ensureAdmin';
 
 export const routes = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 routes.post('/users', createUserController.handle);
+routes.post('/tags', ensureAdmin, createTagController.handle);
